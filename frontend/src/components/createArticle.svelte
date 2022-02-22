cdb7f1db16ff35c127c5d8efe374c6c021b08e42
Frøydis Jørgensen <froydisjo@hotmail.com>
2022-02-22 11:46:11 +0100

    Remove unused file and move code out to component

---
+++ frontend/src/components/createArticle.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Tiptap from '../lib/Tiptap.svelte';
+    import type { IArticle } from '../types/article';
+    import { Status } from '../types/status'
+    let article: IArticle = {title: '', description: null, status: Status.DRAFT};
+    function setTitle(event) {
+        article.title = event.target.value;
+    }
+    async function postArticle () {
+        const res = await fetch('http://localhost:4000/add-article', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(article)})
+        const json = await res.json()
+        let result = JSON.stringify(json)
+        console.log(result)
+    }
+</script>
+
+<main>
+    <section>
+        <div> 
+            <label for="title">Title</label>
+            <input type="text" id="title" value={article.title} on:input={setTitle}/>
+        </div>
+        <Tiptap bind:json={article.description}/>
+    </section>
+    <button on:click={postArticle}>Publish</button>
+</main>
